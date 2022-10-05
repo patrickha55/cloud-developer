03c50a6f2d41ffa5840992df164638c12cdef01a
PatrickHa <pattrickha@gmail.com>
2022-10-05 18:04:40 +0700

    Use env variable for config file

--- course-02/exercises/udacity-c2-restapi/src/sequelize.ts
+++ course-02/exercises/udacity-c2-restapi/src/sequelize.ts
@@ -4,6 +4,8 @@ import { config } from './config/config';
 
 const c = config.dev;
 
+console.log("DB name", c.database);
+
 // Instantiate new Sequelize instance!
 export const sequelize = new Sequelize({
   "username": c.username,
